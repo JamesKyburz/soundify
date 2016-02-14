67c80555e6e3fee2755b0389d27f66834cf8d6da
James Kyburz <james.kyburz@gmail.com>
2016-02-14 14:59:12 +0100

    new spotify api

--- search/spotify.js
+++ search/spotify.js
@@ -2,14 +2,14 @@ var search = require('./search-service')
 module.exports = function (term, cb) {
   var options = {
     callback: cb,
-    parsingKeys: ['tracks', true],
-    uri: 'http://ws.spotify.com/search/1/track.json?q=' + term,
+    parsingKeys: ['tracks', 'items', true],
+    uri: 'https://api.spotify.com/v1/search?q=' + term + '&type=track',
     parseTrack: function (track) {
       var title = track.artists.map(function (x) { return x.name }).join(', ')
       title += ' ' + track.name
-      var ms = parseInt(track.length, 10) * 1000
+      var ms = track.duration_ms
       return {
-        uri: track.href,
+        uri: track.uri,
         title: title,
         source: 'spotify',
         durationMs: ms,
